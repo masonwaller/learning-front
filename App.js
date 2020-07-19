4ab990ef8273e4b3778619f572c71684e005651c
github username <github email address>
2020-07-19 14:45:43 -0700

    got a game board up

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { GameEngine } from "react-native-game-engine";
+import Constants from "./Constants";
 
 export default function App() {
+  const size = Constants.cell_size * Constants.grid_size;
+  let engine = null;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <GameEngine
+        ref={ref => {
+          engine = ref;
+        }}
+        style={{
+          width: size,
+          height: size,
+          flex: null,
+          backgroundColor: "#000000"
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
